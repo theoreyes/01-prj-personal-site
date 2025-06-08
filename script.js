685bbf33e25cd055e602dfdb5d99b6c768444a91
Theodore Anthony Reyes <theodore.anthony.reyes@gmail.com>
2025-06-08 00:07:32 +0000

    Final touches

--- script.js
+++ script.js
@@ -4,9 +4,10 @@
 * Used for generating the projects section dynamically
 */
 
+// Hard-coded project data
 const projects = [
   {title: "💡 Lightbulb Social Media Website",
-   desc: "A collaborative proof-of-concept social media website with features like user profiles " +
+   desc: "A collaborative proof-of-concept social media website with features like user profiles, " +
          "posts, and comments [Site is not currently being hosted]",
    link: "https://github.com/HunterRundhaug/Lightbulb_337"
   },
@@ -17,6 +18,7 @@ const projects = [
   }
 ]
 
+// Loads an individual project into the projectsSection area of the DOM
 function addProject(project, projectsSection) {
   const newSection = document.createElement("div");
   const title = document.createElement("h3");
@@ -26,8 +28,7 @@ function addProject(project, projectsSection) {
   desc.innerText = project.desc;
   link.innerText = "View Project";
   link.href = project.link;
-  link.target = "_blank"; // Opens link in new tab
-
+  link.target = "_blank";
   newSection.appendChild(title);
   newSection.appendChild(desc);
   newSection.appendChild(link);
@@ -35,6 +36,8 @@ function addProject(project, projectsSection) {
   projectsSection.appendChild(newSection);
 }
 
+// Gets project section as DOM element and adds each project to it
+// using data from project json
 const projectsSection = document.querySelector(".projects-row");
 projects.forEach(project => {
   addProject(project, projectsSection);
